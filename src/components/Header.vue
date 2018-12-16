6a1b66d134a3417fcf9ca353b47d3a0e3fdc5375
Justin Sarrasin <sarrasin.justin@gmail.com>
2018-12-16 16:19:32 -0500

    styling added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,55 @@
 <template>
   <div class="header">
-    <h1>New York Times</h1>
+    <h1 class="display-4 text-xs-center font-weight-light">New York Times</h1>
     <form v-on:change.prevent="getNews">
       <select v-model="selected" id="asdf">
         <option v-for="item in categories" :key='item.category'>{{ item.category }}</option>
       </select>
     </form>
 
-    <ul>
-      <li v-for="(data, index) in filteredArticles" :key='index.data'>
-          <h3>{{ data.title }}</h3>
-          <p>{{ data.abstract }}</p>
-          <a :href='data.url' target="_blank">See full article here</a>
-          <!-- <img src="{{ data.image_url }}" alt=""> -->
-      </li>
-    </ul>
+    <v-container fluid grid-list-xl>
+    <v-layout wrap align-center>
+      <v-flex xs12 sm6 d-flex>
+        <v-select v-model="selected" id="asdf">
+          <option v-for="item in categories" :key='item.category'>{{ item.category }}</option>
+      </v-select>
+        
+        <!-- <v-select
+          
+          :items="item in category"
+          label="Select Category"
+          solo
+        ></v-select> -->
+      </v-flex>
+
+
+      <v-flex>
+        <ul style="list-style-type: none; padding: 0">
+          <li v-for="(data, index) in filteredArticles" :key='index.data'>
+            <v-card class="elevation-20">
+              <v-card-title>{{ data.title }}</v-card-title>
+              <v-card-text class="pl-80">{{ data.abstract }}</v-card-text>
+              <v-card-actions class="pl-8">
+                <a class="pl-8" :href='data.url' target="_blank">See full article here</a>
+              </v-card-actions>
+            </v-card>
+          </li>
+        </ul>
+        <!-- <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+          aspect-ratio="2.75"
+        ></v-img> -->
+
+    </v-flex>
+    <v-footer class="pa-3 text-xs-center " fixed>
+      <!-- <v-spacer></v-spacer> -->
+      <div class="text-xs-center">Articles Sourced From The New York Times Top Stories API  &copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
+
+    </v-layout>
+  </v-container>
   </div>
+  
 </template>
 
 <script>
@@ -28,10 +62,32 @@ export default {
       news: [],
       selected: {},
       categories: [
-        { category: 'sports' }, 
         { category: 'home' }, 
+        { category: 'opinion' }, 
         { category: 'world' }, 
-        { category: 'opinion' } 
+        { category: 'national' }, 
+        { category: 'politics' }, 
+        { category: 'upshot' }, 
+        { category: 'nyregion' }, 
+        { category: 'business' }, 
+        { category: 'technology' }, 
+        { category: 'science' }, 
+        { category: 'health' }, 
+        { category: 'sports' }, 
+        { category: 'arts' }, 
+        { category: 'books' }, 
+        { category: 'movies' }, 
+        { category: 'theater' }, 
+        { category: 'sundayreview' }, 
+        { category: 'fashion' }, 
+        { category: 'tmagazine' }, 
+        { category: 'food' }, 
+        { category: 'travel' }, 
+        { category: 'magazine' }, 
+        { category: 'realestate' }, 
+        { category: 'automobiles' }, 
+        { category: 'obituaries' }, 
+        { category: 'insider' }
       ]
     };
   },
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 // import vmApp from './app.vue'
 
 // new Vue(vmApp).$mount('#app')
